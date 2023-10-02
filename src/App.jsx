755d0ac4efc865e52abdeaec9fbaf7564958d21e
giri <mgiricharan@email.com>
2023-10-02 12:10:06 +0530

    hosting price-card

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Price from "./Price";
+// import "./App.css"
+
+function App() {
+  return (
+    <>
+     <Price/>
+    </>
+  );
+}
+
+export default App;
