a07134c0ea9c147d20f0c498ac3e0fdf1d9385d0
giri <mgiricharan@email.com>
2023-10-02 12:40:34 +0530

    price-card

--- src/Price.jsx
+++ src/Price.jsx
@@ -18,10 +18,10 @@ function Price() {
             <h6 className="card-price text-center">$99<span className="period">/month</span></h6>
             <hr/>
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+              <li><span className="fa-li "><i className=" fa-regular fa-circle-check " ></i></span>Single User</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>5GB Storage</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Community Access</li>
               <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
                 Private Projects</li>
               <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
@@ -45,11 +45,11 @@ function Price() {
             <h6 className="card-price text-center">$299<span className="period">/month</span></h6>
             <hr/>
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Single User</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>50GB Storage</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Unlimited
                 Private Projects</li>
               <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
                 Phone Support</li>
@@ -72,16 +72,16 @@ function Price() {
             <h6 className="card-price text-center">$499<span className="period">/month</span></h6>
             <hr/>
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span><strong>Unlimited Users</strong>
               </li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>150GB Storage</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span><strong>Unlimited</strong> Free
                 Subdomains</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
+              <li><span className="fa-li"><i className="fa-regular fa-circle-check"></i></span>Monthly Status Reports</li>
             </ul>
             <div className="d-grid">
               <a href="#" className="btn btn-primary text-uppercase">Subscribe</a>
